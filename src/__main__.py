77bb1b283d32e7ec63cb872d484227cd36bfdb0c
John Toniutti <mrvillager.dev@gmail.com>
2021-04-15 22:44:44 +0200

    Added data serialisation with pickle

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,18 @@
+import logging
+import os
+import pickle
 import api
+
+from typing import List
+
+from objects.song import Song
 from utils import load_folder, create_playlist
 
+PICKLE = "../dump.pickle"
 
-def run():
-    # load and parse files
+
+def load() -> List[Song]:
+    # Load and parse files
     path = input("Where should I search for music files? ")
     musics = list(load_folder(path))
 
@@ -14,8 +23,25 @@ def run():
 
     # Get music features
     spotipy.feature_bulk(musics)
-    create_playlist(musics, "lol.m3u")
+
+    # Save data
+    with open(PICKLE, "wb") as data:
+        pickle.dump(musics, data)
+
+    # Return data
+    return musics
 
 
 if __name__ == "__main__":  # well, it's obvious
-    run()
+    if os.path.isfile(PICKLE):
+        with open(PICKLE, "rb") as data:
+            logging.info("Loading already serialized dataset")
+            songs = pickle.load(data)
+    else:
+        logging.info("Creating dataset")
+        songs = load()
+
+    logging.info("Creating playlist")
+    create_playlist(songs, "../lol.m3u")
+
+    logging.info("Complete")
